2b52235b94cb2f0cb7d05d973a5313c0d80d16f5
neckroxibalba <161411713+neckroxibalba@users.noreply.github.com>
2024-03-04 11:56:43 -0500

    Add files via upload

---
+++ clase002/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Programacion Web</title>    
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/jquery-3.3.1.min"></script>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        
+        <header>            
+            <div class="row">
+              <div class="col-md-5"></div>
+              <div class="col-md-4">
+                <h1><b><p style="color:orange;">NUESTRO TITULO</p></b></h1>    
+              </div>
+              <div class="col-md-3"></div>
+             </div>
+        </header>
+        
+     
+        <div class="row">
+          <div class="col-md-1">1</div>
+          <div class="col-md-10">
+
+            <div class="panel panel-danger">
+                <div class="panel-heading">
+                    <span class="glyphicon glyphicon-education" aria-hidden="true"></span>
+                    <span style="color:orange;"><b>&nbsp;&nbsp;Clase 002</b></span>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10" style="height: 300px">
+                        <div class="row">
+                          <div class="col-md-3">
+                            <nav class="navbar navbar-light bg-light">
+                              <a class="navbar-brand" href="sites/videos.php">
+                                <img src="img/play.png" width="80" height="80" class="d-inline-block align-top" alt="">
+                                1
+                              </a>
+                            </nav>  
+                          </div>                            
+                          <div class="col-md-3">
+                            <nav class="navbar navbar-light bg-light">
+                              <a class="navbar-brand" href="#">
+                                <img src="img/casos.jpg" width="80" height="80" class="d-inline-block align-top" alt="">
+                                2
+                              </a>
+                            </nav>    
+                          </div>
+                          <div class="col-md-3">
+                            <nav class="navbar navbar-light bg-light">
+                              <a class="navbar-brand" href="#">
+                                <img src="img/asistencia.jfif" width="80" height="80" class="d-inline-block align-top" alt="">
+                                3
+                              </a>
+                            </nav>  
+                          </div>
+                          <div class="col-md-3">
+                            <nav class="navbar navbar-light bg-light">
+                              <a class="navbar-brand" href="#">
+                                <img src="img/aplicacion.png" width="80" height="80" class="d-inline-block align-top" alt="">
+                                4
+                              </a>
+                          </nav>  
+                          </div> 
+                        </div>
+                        
+                            
+                            
+                        </div>
+                        <div class="col-md-1"></div>
+                    </div>
+                </div>
+                </div>
+          </div>
+          <div class="col-md-1">3</div>
+       </div>
+
+     
+       
+    </body>
+  
+</html>
